feb732c107a2b28f62950e414444b1d3bfb22b34
hands-rec <hands.records@gmail.com>
2015-01-25 20:37:02 +0900

    amazon report

--- amazon/capybara-amazon.rb
+++ amazon/capybara-amazon.rb
@@ -22,11 +22,19 @@ module Crawler
       click_button "サインイン"
     end
 
-    def potal
+    def portal
       select('',
         :from => '')
       select('昨日', :from => 'preSelectedPeriod')
       first('.line-item-links').click_link("レポート全体を表示")
+    end
+
+    def report
+      within(:xpath, "//*[@class='totals']") do
+        puts "発送済み商品:" + all('td')[1].text
+        puts "売上:" + all('td')[2].text
+        puts "紹介料" + all('td')[3].text
+      end
     end
   end
 end
@@ -34,5 +42,7 @@ end
 
 crawler = Crawler::Amazon.new
 crawler.login
+crawler.portal
+crawler.report
 
 
